e6c49aa80ab5727715c2c69a80067b8209807960
Kostya0955 <3711733@gmail.com>
2019-02-28 21:00:14 +0300

    Make nav-bar

---
+++ src/Components/Carousel.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+export default class Carousel extends React.Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			'image': '',
+			'text': ''
+		}
+		this.interval = null
+		this.index = 0
+		this.showSlide = this.showSlide.bind(this)
+
+	}
+
+	showSlide () {
+		this.setState(this.props.slides[this.index++])
+		if (this.index === this.props.slides.length) {
+			this.index = 0
+		}
+	}
+
+	componentDidMount () {
+		this.interval = setInterval(this.showSlide, 3e3)
+		this.showSlide()
+	}
+
+	componentWillUnmount () {
+		clearInterval(this.interval)
+	}
+
+	render () {
+		return (
+			<div className='columns'>
+				<div className='column is-4'>
+					<figure className='image is-square'>
+						<img src={this.state.image} />
+					</figure>
+				</div>
+				<div className='column is-8'>
+					<p>{this.state.text}</p>
+				</div>
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import FootHeader from './Components/Header/foot-header.js';
+import Footer from './Components/Footer/footer.js';
+import Carousel from './Components/Carousel.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component{
+	render(){
+		return(
+<div>
+<section className="hero is-primary">
+<div class="field">
+  <p class="control has-icons-left">
+    <span class="select">
+      <select>
+        <option selected>Country</option>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </span>
+    <span class="icon is-small is-left">
+      <i class="fas fa-globe"></i>
+    </span>
+  </p>
+</div>
+    <div className="container">
+	    <div className="navbar-brand">
+			<a>
+		  		<img src="https://bulma.io/images/placeholders/128x128.png"/>
+			</a>
+			 <h1 className="title">
+		        Факультет Международных образовательных программ МГТУ им. Н.Э.Баумана
+		      </h1>
+		</div>
+    </div>
+     <FootHeader/>
+</section>
+  <Carousel slides={[
+	{
+		'image': 'https://place-hold.it/500x500&text=slide1',
+		'text': '1'
+	},
+	{
+		'image': 'https://place-hold.it/500x500&text=slide2',
+		'text': '2'
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  ]} />
+	 <div className="box cta">
+        <p class="has-text-centered">
+          <a> LINK 1  </a>
+           <a> LINK 2  </a>
+             <a> LINK 2  </a>
+               <a> LINK 2  </a>
+        </p>
+
+    </div> 
+
+<Footer/>
+
+</div>
+
+		);
+	}
+}
+ReactDOM.render(<App/>, document.getElementById('root'));
+
